a70c236ac093e834c233d98ad531199e05ba0041
Benjamin Bakle <bbakle@apterainc.com>
2016-10-24 15:21:08 -0400

    TDD - Updated to expect literal values as comparison

--- MergeLists.Tests/spec/merge-lists.spec.js
+++ MergeLists.Tests/spec/merge-lists.spec.js
@@ -18,127 +18,62 @@
     });
 
     describe("given the decending flag is set to true", function () {
-        //var fixture, sourceList, targetList;
-        //fixture = setUpHTMLFixture();
-        //sourceList = fixture.find(".list1");
-        //targetList = fixture.find(".list2");
         beforeEach(function () {
-            expectedResult = sourceList.clone();
             mergeLists(sourceList, targetList, 1, true);
-
         });
         it("reverses the list items", function () {
-            reverseList(expectedResult);
-            addClassToListItems(expectedResult, "appended-item");
-            expect(targetList.html()).toContain(expectedResult.html());
+            expect(targetList.html()).toContain('<li class="appended-item">List 1 Item 3</li><li class="appended-item">List 1 Item 2</li><li class="appended-item">List 1 Item 1</li>');
         });
     })
 
     describe("given the decending flag is set to false", function () {
         beforeEach(function () {
-            expectedResult = sourceList.clone();
             var decending = false;
             mergeLists(sourceList, targetList, 1, decending);
         });
         it("it does not reverses the list items", function () {
-            addClassToListItems(expectedResult, "appended-item");
-            expect(targetList.html()).toContain(expectedResult.html());
+            expect(targetList.html()).toContain('<li class="appended-item">List 1 Item 1</li><li class="appended-item">List 1 Item 2</li><li class="appended-item">List 1 Item 3</li>');
         });
     })
 
     describe("given a target list is empty", function () {
         beforeEach(function () {
-            fixture = setUpHTMLFixture();
-            sourceList = fixture.find(".list1");
             targetList = $("<ul></ul>")
-            expectedResult = sourceList.clone();
             mergeLists(sourceList, targetList, 1, false);
         });
         it("adds source list items to target list", function () {
-            expectedResult.append(listItems(sourceList));
-            addClassToListItems(expectedResult, "appended-item");
-            expect(targetList.html()).toEqual(expectedResult.html());
+            expect(targetList.html()).toEqual('<li class="appended-item">List 1 Item 1</li><li class="appended-item">List 1 Item 2</li><li class="appended-item">List 1 Item 3</li>');
         });
-
-
     });
 
     describe("given a target list is not empty", function () {
         var position;
         describe("and position is 1", function () {
             beforeEach(function () {
-                fixture = setUpHTMLFixture();
-                sourceList = fixture.find(".list1");
-                targetList = fixture.find(".list2");
-                expectedResult = sourceList.clone();
                 position = 1;
                 mergeLists(sourceList, targetList, position, false);
             });
             it("inserts source list items before the target list's first item", function () {
-                var insertedCorrectly = true;
-                for (i = 1; i < 4; i++) {
-                    insertedCorrectly = $("> li:nth-child(" + i + ")", targetList).html() == $("> li:nth-child(" + i + ")", expectedResult).html();
-                }
-                expect(insertedCorrectly).toEqual(true);
+                expect($("> li:first-child", targetList).html()).toEqual("List 1 Item 1");
+                expect($("> li:nth-child(2)", targetList).html()).toEqual("List 1 Item 2");
+                expect($("> li:nth-child(3)", targetList).html()).toEqual("List 1 Item 3");
             });
 
         });
         describe("and position is greater than 1", function () {
             beforeEach(function () {
-                fixture = setUpHTMLFixture();
-                sourceList = fixture.find(".list1");
-                targetList = fixture.find(".list2");
-                expectedResult = sourceList.clone();
-               position = 2;
+                position = 2;
                 mergeLists(sourceList, targetList, position, false);
             });
             it("inserts source list items after the target list's item at given position", function () {
-                var insertedCorrectly = true;
-                for (i = position; i < position + 3; i++) {
-                    insertedCorrectly = $("> li:nth-child(" + i + ")", targetList).html() == $("> li:nth-child(" + (i - 1) + ")", expectedResult).html();
-                }
-                expect(insertedCorrectly).toEqual(true);
-
+                expect($("> li:first-child", targetList).html()).toEqual("List 2 Item 1");
+                expect($("> li:nth-child(2)", targetList).html()).toEqual("List 1 Item 1");
+                expect($("> li:nth-child(3)", targetList).html()).toEqual("List 1 Item 2");
+                expect($("> li:nth-child(4)", targetList).html()).toEqual("List 1 Item 3");
             });
-
-
         });
     });
 
-});
-
-
-
-
-
-
-xdescribe("The addListItemToList function", function () {
-    var fixture, sourceList, targetList;
-    describe("given list Item target position is less than target list length", function () {
-        beforeEach(function () {
-            var item = $("<li>New Item</li>");
-            fixture = setUpHTMLFixture();
-            targetList = fixture.find('.list2');
-            addListItemToList(item, targetList, 2);
-        });
-        it("insert list item at target position", function () {
-            expect($("li:nth-child(2)", targetList)).toEqual(item);
-        });
-    });
-    describe("given list Item target position is greater than target list length", function () {
-        beforeEach(function () {
-            var item = $("<li>New Item</li>");
-            fixture = setUpHTMLFixture();
-            targetList = fixture.find('.list2');
-            var position = listLength(targetList) + 1;
-            addListItemToList(item, targetList, position);
-        });
-        it("insert list item in last position of target list", function () {
-            expect($("li:last-child", targetList)).toEqual(item);
-
-
-        });
-    });
 });
 
 function setUpHTMLFixture() {
--- MergeLists/merge-lists.js
+++ MergeLists/merge-lists.js
@@ -1,42 +1,47 @@
-﻿function mergeLists(ulInsert, ulInsertInto, position, decending) {
-    addClassToListItems(ulInsert, "appended-item");
-    decending && reverseList(ulInsert);
-    var ulItems = listItems(ulInsert);
-    if (listLength(ulInsertInto) == 0) {
-        ulInsertInto.append(ulItems);
-    } else {
-        (position == 1) ? ulItems.insertBefore($('> li:first-child', ulInsertInto)) : ulItems.insertAfter($('> li:nth-child(' + (position - 1) + ')', ulInsertInto))
+﻿(function (window) {
+    window.mergeLists = mergeLists;
+    function mergeLists(ulInsert, ulInsertInto, position, decending) {
+        addClassToListItems(ulInsert, "appended-item");
+        decending && reverseList(ulInsert);
+        var ulItems = listItems(ulInsert);
+        if (listLength(ulInsertInto) == 0) {
+            ulInsertInto.append(ulItems);
+        } else {
+            (position == 1) ? ulItems.insertBefore($('> li:first-child', ulInsertInto)) : ulItems.insertAfter($('> li:nth-child(' + (position - 1) + ')', ulInsertInto))
+        }
     }
-}
-//::  HELPER FUNCTIONS :://
-function listLength(ul) {
-    return ul.find(">li").length;
-}
 
-function listItems(ul) {
-    return $('> li', ul);
-}
+    //::  HELPER FUNCTIONS :://
+    function listLength(ul) {
+        return ul.find(">li").length;
+    }
 
-function listIsEmpty(ul) {
-    return listLength(ul) == 0;
-}
+    function listItems(ul) {
+        return $('> li', ul);
+    }
 
 
-//::  HELPER METHODS :://
-function addListItemToList(listItem, ul, position) {
-    var ul_len = listLength(ul);
-    (position > ul_len) ? $("> li:last-child", ul).after(listItem) : $("> li:nth-child(" + position + ")", ul).before(listItem);
-}
-function addClassToListItems(ul, className) {
-    $("li", ul).each(function () {
-        $(this).addClass(className);
-    });
-}
+    //::  HELPER METHODS :://
+    function addListItemToList(listItem, ul, position) {
+        var ul_len = listLength(ul);
+        (position > ul_len) ? $("> li:last-child", ul).after(listItem) : $("> li:nth-child(" + position + ")", ul).before(listItem);
+    }
+    function addClassToListItems(ul, className) {
+        $("li", ul).each(function () {
+            $(this).addClass(className);
+        });
+    }
 
-function reverseList(ul) {
-    ul.children().each(function (i, li) {
-        ul.addClass("reversed").prepend(li)
-    });
-}
+    function reverseList(ul) {
+        ul.children().each(function (i, li) {
+            ul.addClass("reversed").prepend(li)
+        });
+    }
 
+
+})(window);
+
+//function listIsEmpty(ul) {
+//    return listLength(ul) == 0;
+//}
 
